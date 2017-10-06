183e9031bc8eeb6a937a280b7d4995f2296ed14a
Clinton Wang <clintonjwang@gmail.com>
2017-10-06 18:00:05 -0400

    api update

--- api_queries.py
+++ api_queries.py
@@ -1,6 +1,8 @@
 import requests
+from dicom.examples import anonymize
+import os
 
-def search_vna(user, pw, accNum=None, study=None, series=None, region='test', limit=None):
+def search_vna(user, pw, accNum=None, study=None, series=None, region='test', limit=None, modality="MR"):
     if region == 'test':
         host = 'vnatest1vt'
         port = '8083'
@@ -22,8 +24,8 @@ def search_vna(user, pw, accNum=None, study=None, series=None, region='test', li
 
         if series is not None:
             url += "/" + series + "/instances"
-        else:
-            url += "?Modality=MR"
+        elif modality is not None:
+            url += "?Modality=" + modality
 
     if limit is not None:
         if "?" in url:
@@ -65,6 +67,17 @@ def retrieve_vna(user, pw, filename, study=None, series=None, instance=None, reg
 
         url += "/metadata"
 
+        r = requests.get(url, auth=(user, pw)) #, headers=headers
+
+        if r.status_code != 200:
+            print("Skipped series:", series)
+            #raise ValueError("Invalid request (response code %d) for URL: %s" % (r.status_code, url))
+            return None, url
+
+        with open(filename, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+
     else:
         url = ''.join(['http://', host, ':', port,
                        "/AcuoREST/wadoget?requestType=WADO&contentType=application/dicom&studyUID=",
@@ -75,13 +88,20 @@ def retrieve_vna(user, pw, filename, study=None, series=None, instance=None, reg
             if instance is not None:
                 url += "&objectUID=" + instance
 
-    r = requests.get(url, auth=(user, pw)) #, headers=headers
+        r = requests.get(url, auth=(user, pw)) #, headers=headers
 
-    with open(filename, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=128):
-            fd.write(chunk)
+        if r.status_code != 200:
+            #print("Skipped instance:", instance)
+            #raise ValueError("Invalid request (response code %d) for URL: %s" % (r.status_code, url))
+            return None, url
+
+        with open("temp.dcm", 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+
+        anonymize.anonymize(filename="temp.dcm", output_filename=filename)
+
+        os.remove("temp.dcm")
             
-    #if r.status_code != 200:
-        #raise ValueError("Invalid request (response code %d) for URL: %s" % (r.status_code, url))
         
     return r, url
