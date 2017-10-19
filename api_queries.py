938d1879516ceb93616b7c51462418c3314aaac1
Clinton Wang <clintonjwang@gmail.com>
2017-10-19 15:48:32 -0400

    checkpoint before merge into voi-classifier

--- api_queries.py
+++ api_queries.py
@@ -4,12 +4,12 @@ import importlib
 import os
 import time
 
-def search_vna(user, pw, accNum=None, study=None, series=None, region='test', limit=None, modality="MR"):
+def search_vna(user, pw, acc_num=None, study=None, series=None, region='test', limit=None, modality="MR"):
     if region == 'test':
         host = 'vnatest1vt'
         port = '8083'
     elif region == 'prod':
-        host = '10.47.11.220'
+        host = '10.47.11.221'
         port = '8083'
     else:
         raise ValueError("Unsupported region")
@@ -18,8 +18,8 @@ def search_vna(user, pw, accNum=None, study=None, series=None, region='test', li
     url = ''.join(['http://', host, ':', port,
                    "/AcuoREST/dicomrs/search/studies"])
 
-    if accNum is not None:
-        url += "?AccessionNumber=" + accNum
+    if acc_num is not None:
+        url += "?AccessionNumber=" + acc_num
 
     elif study is not None:
         url += "/" + study + "/series"
@@ -44,14 +44,14 @@ def search_vna(user, pw, accNum=None, study=None, series=None, region='test', li
     return r, url
 
 
-def retrieve_vna(user, pw, filename, study=None, series=None, instance=None, region='test', metadata=False, protocolExclude=None):
+def retrieve_vna(user, pw, filename, study=None, series=None, instance=None, region='test', metadata=False):
     """If metadata is true, filename should end in xml. Else end in dcm."""
 
     if region == 'test':
         host = 'vnatest1vt'
         port = '8083'
     elif region == 'prod':
-        host = '10.47.11.220'
+        host = '10.47.11.221'
         port = '8083'
     else:
         raise ValueError("Unsupported region")
@@ -109,52 +109,91 @@ def retrieve_vna(user, pw, filename, study=None, series=None, instance=None, reg
 
 
 if __name__ == "__main__":
-    user = ''
+    user = 'wangkcc4'
     pw = ''
     region = 'prod'
 
     accNum = 'TESTSIRIANO'
-    r, url = search_vna(user, pw, region=region, accNum=accNum)
+    lesion_type = 'fnh'
+    r, url = search_vna(user, pw, region=region, acc_num=acc_num)
     study = r.json()[0]['0020000D']['Value'][0]
 
-    r, url = search_vna(user, pw, region=region, study=study, modality=None)
+    r, url = search_vna(user, pw, region=region, study=study)
     study_info = r.json()
-    series = [ser['0020000E']['Value'][0] for ser in study_info]
+    series = list(set([ser['0020000E']['Value'][0] for ser in study_info]))
 
     instances = {}
 
     for ser in series:
-        r, url = search_vna(user, pw, region=region, study=study, series=ser)
+        r, url = aq.search_vna(user, pw, region=region, study=study, series=ser)
         series_info = r.json()
         instances[ser] = [inst['00080018']['Value'][0] for inst in series_info]
-        if len(instances[ser]) < 5:
+        if len(instances[ser]) < 10:
+            series.remove(ser)
+            print("Skipping series", ser)
             del instances[ser]
+            
 
-    base_dir = "raw_imgs\\" + accNum
+    base_dir = "raw_imgs\\" + lesion_type + "\\" + acc_num
+    protocolExclude = ["cor", "localizer", "resp", "sub", "phase", "tse", "epi", "bolus", "ep2d", "dwi", "opp", "ept"]
 
+    if not os.path.exists("raw_imgs\\" + lesion_type):
+        os.makedirs("raw_imgs\\" + lesion_type)
+        
     if not os.path.exists(base_dir):
         os.makedirs(base_dir)
+        
+    if not os.path.exists(base_dir+"\\others"):
+        os.makedirs(base_dir+"\\others")
         
     total = 0
     skip_ser = 0
     skip_inst = 0
+    rmdir = []
+            
+    t = time.time()
     for ser in instances:
-        t = time.time()
         print("\n==============")
-        print("Loading series", ser)
+        print("Loading metadata for series", ser)
         img_dir = base_dir + "\\" + ser
         
         if not os.path.exists(img_dir):
             os.makedirs(img_dir)
             
-        r, url = retrieve_vna(user, pw, region=region, filename = img_dir+"\\metadata.xml",
+        r, url = aq.retrieve_vna(user, pw, region=region, filename = img_dir+"\\metadata.xml",
                           study=study, series=ser, metadata=True)
         if r is None:
             skip_ser += 1
+            print("Skipping series.")
             continue
             
+        txt = r.text
+        search = '<DicomAttribute tag="0008103E" vr="LO" keyword="SeriesDescription">\r\n      <Value number="1">'
+        index = txt.find(search) + len(search)
+        protocolName = txt[index:index + txt[index:].find("</Value>")].lower()
+        
+        while os.path.exists(base_dir + "\\" + protocolName):
+            protocolName += "+"
+        os.rename(img_dir, base_dir + "\\" + protocolName)
+        img_dir = base_dir + "\\" + protocolName
+        
+        
+        skip = False
+        for excKeyword in protocolExclude:
+            if excKeyword in protocolName:
+                skip_ser += 1
+                skip = True
+                print("Skipping images for series with description", protocolName)
+                rmdir.append(protocolName)
+                break
+                
+        if skip:
+            continue
+            
+        print("Loading images for series with description:", protocolName)
+            
         for count, inst in enumerate(instances[ser]):
-            r, url = retrieve_vna(user, pw, region=region, filename = img_dir+"\\"+str(count)+".dcm",
+            r, _ = aq.retrieve_vna(user, pw, region=region, filename = img_dir+"\\"+str(count)+".dcm",
                           study=study, series=ser, instance=inst)
             
             if r is not None:
@@ -162,8 +201,11 @@ if __name__ == "__main__":
             print(".", end="")
         
         total += count
-        print("\nTime elapsed: %.2fs" % time.time()-t)
+        
+    for d in rmdir:
+        os.rename(base_dir + "\\" + d, base_dir + "\\others\\" + d)
         
     print("Series loaded: ", len(series)-skip_ser, "/", len(series), sep="")
     print("Total images loaded:", total)
     print("Images skipped:", skip_inst)
+    print("\nTime elapsed: %.2fs" % (time.time()-t))
